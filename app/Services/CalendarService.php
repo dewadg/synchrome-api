030e2f79ad4d269cf2ae7a08dd90e8f71faf8ac8
Adhyaksa Jaya <adhyaksaj@gmail.com>
2018-12-14 15:17:00 +0700

    Added CalendarService and its test cases

---
+++ app/Services/CalendarService.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Services;
+
+use App\Calendar;
+use App\CalendarEvent;
+use App\Repositories\AttendanceTypeRepo;
+use App\Repositories\CalendarRepo;
+use Illuminate\Support\Collection;
+
+class CalendarService
+{
+    /**
+     * @var CalendarRepo
+     */
+    protected $repo;
+
+    /**
+     * @var AttendanceTypeRepo
+     */
+    protected $attendance_type_repo;
+
+    /**
+     * CalendarService constructor.
+     */
+    public function __construct()
+    {
+        $this->repo = new CalendarRepo;
+        $this->attendance_type_repo = new AttendanceTypeRepo;
+    }
+
+    /**
+     * Creates new calendar.
+     *
+     * @param array $data
+     * @param array $events
+     * @return Calendar
+     */
+    public function create(array $data, array $events = [])
+    {
+        $calendar = Calendar::create([
+            'name' => $data['name'],
+            'start' => $data['start'],
+            'end' => $data['end'],
+        ]);
+
+        $events_data = collect($events)
+            ->map(function ($item) {
+                return new CalendarEvent([
+                    'name' => $item['name'],
+                    'start' => $item['start'],
+                    'end' => ! isset($item['end']) ? null : $item['end'],
+                    'attendance_type_id' => $item['attendance_type_id'],
+                ]);
+            })
+            ->all();
+
+        $calendar->events()->saveMany($events_data);
+        $calendar->load('events');
+
+        return $calendar;
+    }
+
+    /**
+     * Updates a calendar.
+     *
+     * @param $id
+     * @param array $data
+     * @return Calendar
+     */
+    public function update($id, array $data)
+    {
+        $calendar = $this->repo->find($id);
+
+        $calendar->update([
+            'name' => $data['name'],
+            'start' => $data['start'],
+            'end' => $data['end'],
+        ]);
+
+        return $calendar;
+    }
+
+    /**
+     * Returns new calendar by ID.
+     *
+     * @param $id
+     * @return Calendar
+     */
+    public function find($id)
+    {
+        return $this->repo->find($id);
+    }
+
+    /**
+     * Deletes calendar by ID.
+     *
+     * @param $id
+     * @return boolean
+     */
+    public function delete($id)
+    {
+        return $this->repo->delete($id);
+    }
+
+    /**
+     * Returns events of a calendar.
+     *
+     * @param $id
+     * @return Collection
+     */
+    public function getEvents($id)
+    {
+        $calendar = $this->repo->find($id);
+
+        return $calendar->events;
+    }
+
+    /**
+     * Add an event to a calendar.
+     *
+     * @param $id
+     * @param array $event
+     * @return Calendar
+     */
+    public function addEvent($id, array $event)
+    {
+        $calendar = $this->repo->find($id);
+
+        $calendar->events()->create([
+            'name' => $event['name'],
+            'start' => $event['start'],
+            'end' => ! isset($event['end']) ? null : $event['end'],
+            'attendance_type_id' => $event['attendance_type_id'],
+        ]);
+
+        $calendar->load('events');
+
+        return $calendar;
+    }
+
+    /**
+     * Updates an event.
+     *
+     * @param $calendar_id
+     * @param  $event_id
+     * @param array $data
+     * @return Calendar
+     */
+    public function updateEvent($calendar_id, $event_id, array $data)
+    {
+        $calendar = $this->repo->find($calendar_id);
+        $event = $calendar->events()->find($event_id);
+        
+        $event->update([
+            'name' => $data['name'],
+            'start' => $data['start'],
+            'end' => ! isset($data['end']) ? null : $data['end'],
+            'attendance_type_id' => $data['attendance_type_id'],
+        ]);
+
+        $calendar->load('events');
+
+        return $calendar;
+    }
+
+    /**
+     * Deletes an event.
+     *
+     * @param $calendar_id
+     * @param $event_id
+     * @return boolean
+     */
+    public function deleteEvent($calendar_id, $event_id)
+    {
+        return CalendarEvent::where([
+            'id' => $event_id,
+            'calendar_id' => $calendar_id,
+        ])->delete() === 1 ? true : false;
+    }
+}
---
+++ tests/CalendarServiceTest.php
@@ -0,0 +1,197 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use App\Services\CalendarService;
+
+class RoleServiceTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @var [type]
+     */
+    protected $service;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->service = new CalendarService;
+    }
+
+    public function testCreate()
+    {
+        $data = [
+            'name' => 'Kalender Kerja 2017',
+            'start' => '2018-01-01',
+            'end' => '2018-12-31',
+        ];
+
+        $events = [
+            [
+                'name' => 'Tahun Baru 2018',
+                'start' => '2018-01-01',
+                'attendance_type_id' => 'L',
+            ],
+            [
+                'name' => 'Natal 2018',
+                'start' => '2018-12-25',
+                'attendance_type_id' => 'L',
+            ],
+        ];
+
+        $calendar = $this->service->create($data, $events);
+
+        $this->assertInstanceOf(App\Calendar::class, $calendar);
+        $this->assertInstanceOf(App\CalendarEvent::class, $calendar->events->first());
+    }
+
+    public function testFind()
+    {
+        $data = [
+            'name' => 'Kalender Kerja 2017',
+            'start' => '2018-01-01',
+            'end' => '2018-12-31',
+        ];
+
+        $events = [
+            [
+                'name' => 'Tahun Baru 2018',
+                'start' => '2018-01-01',
+                'attendance_type_id' => 'L',
+            ],
+            [
+                'name' => 'Natal 2018',
+                'start' => '2018-12-25',
+                'attendance_type_id' => 'L',
+            ],
+        ];
+
+        $new_calendar = $this->service->create($data, $events);
+
+        $calendar = $this->service->find($new_calendar->id);
+
+        $this->assertInstanceOf(App\Calendar::class, $calendar);
+        $this->assertInstanceOf(App\CalendarEvent::class, $calendar->events->first());
+    }
+
+    public function testDelete()
+    {
+        $data = [
+            'name' => 'Kalender Kerja 2017',
+            'start' => '2018-01-01',
+            'end' => '2018-12-31',
+        ];
+
+        $events = [
+            [
+                'name' => 'Tahun Baru 2018',
+                'start' => '2018-01-01',
+                'attendance_type_id' => 'L',
+            ],
+            [
+                'name' => 'Natal 2018',
+                'start' => '2018-12-25',
+                'attendance_type_id' => 'L',
+            ],
+        ];
+
+        $new_calendar = $this->service->create($data, $events);
+
+        $this->assertTrue($this->service->delete($new_calendar->id));
+    }
+
+    public function testUpdate()
+    {
+        $data = [
+            'name' => 'Kalender Kerja 2017',
+            'start' => '2017-01-01',
+            'end' => '2017-12-31',
+        ];
+
+        $new_data = [
+            'name' => 'Kalender Kerja 2018',
+            'start' => '2018-01-01',
+            'end' => '2018-12-31',
+        ];
+
+        $new_calendar = $this->service->create($data);
+        $updated_calendar = $this->service->update($new_calendar->id, $new_data);
+
+        $this->assertTrue($updated_calendar->name === $new_data['name']);
+        $this->assertTrue($updated_calendar->start->format('Y-m-d') === $new_data['start']);
+        $this->assertTrue($updated_calendar->end->format('Y-m-d') === $new_data['end']);
+    }
+
+    public function testAddEvent()
+    {
+        $data = [
+            'name' => 'Kalender Kerja 2017',
+            'start' => '2018-01-01',
+            'end' => '2018-12-31',
+        ];
+
+        $new_calendar = $this->service->create($data);
+
+        $updated_calendar = $this->service->addEvent($new_calendar->id, [
+            'name' => 'Tahun Baru 2018',
+            'start' => '2018-01-01',
+            'attendance_type_id' => 'L',
+        ]);
+
+        $this->assertTrue($updated_calendar->events->first()->name === 'Tahun Baru 2018');
+        $this->assertTrue($updated_calendar->events->first()->start->format('Y-m-d') === '2018-01-01');
+        $this->assertTrue($updated_calendar->events->first()->attendance_type_id === 'L');
+    }
+
+    public function testUpdateEvent()
+    {
+        $data = [
+            'name' => 'Kalender Kerja 2017',
+            'start' => '2018-01-01',
+            'end' => '2018-12-31',
+        ];
+
+        $events = [
+            [
+                'name' => 'Tahun Baru 2018',
+                'start' => '2018-01-01',
+                'attendance_type_id' => 'L',
+            ],
+        ];
+
+        $new_calendar = $this->service->create($data, $events);
+        $updated_calendar = $this->service->updateEvent(
+            $new_calendar->id,
+            $new_calendar->events->first()->id,
+            [
+                'name' => 'New Year 2018',
+                'start' => '2018-01-01',
+                'attendance_type_id' => 'L',
+            ]
+        );
+
+        $this->assertTrue($updated_calendar->events->first()->name === 'New Year 2018');
+    }
+
+    public function testDeleteEvent()
+    {
+        $data = [
+            'name' => 'Kalender Kerja 2017',
+            'start' => '2018-01-01',
+            'end' => '2018-12-31',
+        ];
+
+        $events = [
+            [
+                'name' => 'Tahun Baru 2018',
+                'start' => '2018-01-01',
+                'attendance_type_id' => 'L',
+            ],
+        ];
+
+        $new_calendar = $this->service->create($data, $events);
+
+        $this->assertTrue($this->service->deleteEvent($new_calendar->id, $new_calendar->events->first()->id));
+    }
+}
