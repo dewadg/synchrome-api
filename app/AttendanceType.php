3b56783a33119c5df0aad60cf71003027a454251
Adhyaksa Jaya <adhyaksaj@gmail.com>
2018-12-14 16:07:32 +0700

    Fixed bug on AttendanceType

--- app/AttendanceType.php
+++ app/AttendanceType.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class AttendanceType extends Model
 {
+    public $incrementing = false;
+    
     /**
      * Attributes that are mass assignable.
      *
