7edbf2769c4895da39b8c3430fc978f29183e931
Adhyaksa Jaya <adhyaksaj@gmail.com>
2018-12-14 15:16:49 +0700

    Enabled soft delete on Calendar

--- app/Calendar.php
+++ app/Calendar.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Calendar extends Model
 {
+    use SoftDeletes;
+    
     /**
      * Attributes that are mass assignable.
      *
