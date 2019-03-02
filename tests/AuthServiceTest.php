11924a2ff086678e4a104a9e6d6d77873e8f0813
Adhyaksa Jaya <adhyaksaj@gmail.com>
2019-03-02 15:59:45 +0700

    Revamped AuthServiceTest

--- tests/AuthServiceTest.php
+++ tests/AuthServiceTest.php
@@ -1,27 +1,37 @@
 <?php
 
-use Laravel\Lumen\Testing\DatabaseTransactions;
 use App\Services\AuthService;
+use Laravel\Lumen\Testing\DatabaseMigrations;
 
 class AuthServiceTest extends TestCase
 {
-    use DatabaseTransactions;
+    use DatabaseMigrations;
+
+    protected $test_auth_service;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $seeder = app(DatabaseSeeder::class);
+        $seeder->call('AccessesTableSeeder');
+        $seeder->call('RolesTableSeeder');
+        $seeder->call('UsersTableSeeder');
+
+        $this->test_auth_service = new AuthService;
+    }
 
     public function testAuthenticate()
     {
-        $service = new AuthService;
-
-        $access_token = $service->authenticate('su', 'supersu');
+        $access_token = $this->test_auth_service->authenticate('su', 'supersu');
 
         $this->assertInstanceOf(\App\AccessToken::class, $access_token);
     }
 
     public function testAuthorize()
     {
-        $service = new AuthService;
-
-        $access_token = $service->authenticate('su', 'supersu');
-        $user = $service->authorize($access_token->username, $access_token->password);
+        $access_token = $this->test_auth_service->authenticate('su', 'supersu');
+        $user = $this->test_auth_service->authorize($access_token->username, $access_token->password);
 
         $this->assertTrue($user->id === $access_token->user->id);
     }
