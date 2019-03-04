6b3dc85f658fd40a5a9b6c4f381387c6a7fd70cb
Adhyaksa Jaya <adhyaksaj@gmail.com>
2019-03-04 21:58:50 +0700

    Call 2 new seeders on DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,7 @@ class DatabaseSeeder extends Seeder
          $this->call(RolesTableSeeder::class);
          $this->call(UsersTableSeeder::class);
          $this->call(AttendanceTypesTableSeeder::class);
+         $this->call(EchelonTypesTableSeeder::class);
+         $this->call(EchelonsTableSeeder::class);
     }
 }
